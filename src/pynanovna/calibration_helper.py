79acd41491b10c09a706bb400ef6eb2691c00e8a
tbergkvist <bergkvist.teo@protonmail.com>
2024-11-26 11:53:37 +0100

    Add comments to calibration_helper.py

--- src/pynanovna/calibration_helper.py
+++ src/pynanovna/calibration_helper.py
@@ -1,9 +1,15 @@
+# fmt: off
 import time
 
 from pynanovna import VNA
 
+###################### Note ######################
+# This script can be run as a calibration helper.
+# It can also be used as an example on
+# how to use the calibation functions.
+##################################################
 
-vna = VNA()
+vna = VNA() #  Create a vna object.
 
 if not vna.is_connected():
     print("No vna connected. Quitting.")
@@ -14,12 +20,12 @@ start = float(input("Enter minimum frequency as a float: "))
 stop = float(input("Enter maximum frequency as a float: "))
 points = int(input("Enter number of points as an int: "))
 
-vna.set_sweep(start, stop, points)
+vna.set_sweep(start, stop, points) # It is important to perform the calibration in the frequency range you intend to measure in.
 
 input(
     "Calibrate short.\n\nPlease connect the short standard to port 1 of the NanoVNA.\n\n Press enter when you are ready to continue."
 )
-vna.calibration_step("short")
+vna.calibration_step("short") #  The calibration is done in several steps as usual.
 
 input(
     "Calibrate open.\n\nPlease connect the open standard to port 1 of the NanoVNA.\n\nEither use a supplied open, or leave the end of the cable unconnected if desired.\n\nPress enter when you are ready to continue."
@@ -44,7 +50,7 @@ vna.calibration_step("through")
 input(
     "Calibrate complete.\nThe calibration process is now complete. Press enter to apply the calibration parameters."
 )
-vna.calibrate()
+vna.calibrate() #  When all steps are done, run the calibrate function to calculate a calibration and enable it.
 
 ans = input("Do you wish to save this calibration to a file? [y]/n: ")
 
